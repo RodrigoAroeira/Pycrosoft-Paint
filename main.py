03b15f23f2bf7fc8b73cf7fbbdc806b1ba2a3c62
RodrigoAroeira <raroeirabr@gmail.com>
2024-09-12 15:06:09 -0300

    Fixed formatting and added match case under pg.KEYDOWN event

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
-from Assets import *
-import pygame as pg
-from functools import partial
+from Assets import WIDTH, HEIGHT, make_grid, load_grid, draw, get_pos, save
+from typing import TYPE_CHECKING
 from copy import deepcopy
+import pygame as pg
 import sys
-WIDTH = HEIGHT = 800
+
+if TYPE_CHECKING:
+    from Assets import Tile
+
 
 ROOT = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption("Pycrosoft Paint")
 
-make_grid = partial(make_grid, root=ROOT)
 
 def main():
     ROWS = 100
-    grid = make_grid(ROWS, HEIGHT)
+    grid = make_grid(ROWS, HEIGHT, ROOT)
 
     run = True
-    saved: list = load_grid(ROWS, HEIGHT, ROOT)
+    saved: list[Tile] = load_grid(ROWS, HEIGHT, ROOT)
     while run:
         draw(ROOT, grid)
         for event in pg.event.get():
             if event.type == pg.QUIT or pg.key.get_pressed()[pg.K_ESCAPE]:
                 run = False
-                
+
             if pg.mouse.get_pressed()[0] and pg.mouse.get_pressed()[2]:
                 pos = pg.mouse.get_pos()
                 row, col = get_pos(pos, ROWS, HEIGHT)
@@ -49,20 +51,21 @@ def main():
                 tile.green()
 
             if event.type == pg.KEYDOWN:
-                if event.key == pg.K_s:
-                    print("Saving...")
-                    saved = deepcopy(grid)
-                    if any(arg for arg in sys.argv if arg == "-save"):
-                        save(saved)
-                if event.key == pg.K_l:
-                    grid = deepcopy(saved)
-                    print("Loading...")
+                match event.key:
+                    case pg.K_s:
+                        print("Saving...")
+                        saved = deepcopy(grid)
+                        if any(arg for arg in sys.argv if arg == "-save"):
+                            save(saved)
 
-                if event.key == pg.K_r:
-                    grid = make_grid(ROWS, HEIGHT)
+                    case pg.K_l:
+                        grid = deepcopy(saved)
+                        print("Loading...")
 
-            
+                    case pg.K_r:
+                        grid = make_grid(ROWS, HEIGHT, ROOT)
+
+
 if __name__ == "__main__":
 
     main()
-    
